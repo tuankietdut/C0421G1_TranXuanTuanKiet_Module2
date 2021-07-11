c75433fdb84bba918931acaf99cf66a6687d23fd
tuankietdut <tuankietdut@gmail.com>
2021-07-11 19:50:00 +0700

    update frist 11/07 commit

---
+++ Module_2/src/Test/LambaEx.java
@@ -0,0 +1,15 @@
+package Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LambaEx {
+    public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        StringFunction
+    }
+}
